b03b24c4a824d580318be3bb20d1a5e588034912
mirayuluduz <miray.uluduz@yuceyazilim.com>
2021-02-07 17:39:39 +0300

    initial commit

---
+++ packages/project-3/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const port = process.env.PORT || 9000;
+const app = express();
+
+app.get("/", (re, res) => {
+  res.send("Proje 3 Benim");
+});
+app.listen(port, (err) => {
+  if (err) {
+    console.log(`Error: ${err.message}`);
+  } else {
+    console.log(`Listening on port ${port}`);
+  }
+});
